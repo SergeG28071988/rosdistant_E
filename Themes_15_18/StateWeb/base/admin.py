4bfc24a0e827f7a5b5b005d63b344f67548994a2
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-02-14 17:37:39 +0300

    Создали и зарегистрировали модели

--- Themes_15_18/StateWeb/base/admin.py
+++ Themes_15_18/StateWeb/base/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from .models import City, Country, Human
+
 # Register your models here.
+
+
+class CityAdmin(admin.ModelAdmin):
+    pass
+
+
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+class HumanAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(City, CityAdmin)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Human, HumanAdmin)
--- Themes_15_18/StateWeb/base/models.py
+++ Themes_15_18/StateWeb/base/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    population = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    area = models.PositiveIntegerField()
+    population = models.PositiveIntegerField()
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Human(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    date_birth = models.DateField()
+    place_residence = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
